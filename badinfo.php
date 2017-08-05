4b9e7f190a619569d2bc53fa9669f7ee0b3dccd9
mp657 <mp657@njit.edu>
2017-08-05 17:39:09 -0400

    clearing cookies and wrong login credentials error message

---
+++ badinfo.php
@@ -0,0 +1,4 @@
+<?php
+// user will be directed to this page with error message when this is called in case of wrong user name and password validation
+  echo "wrong username and password please try again? <br/>";
+?>
